5a5e0ecc180d450f2fb825a327cbdc6d44bbc285
Domingo John Ruselle R – MBTC Contractual <t-jrdomingo@mbtc.mgc.local>
2022-10-28 17:28:46 +0800

    Student Controller

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # springboot-restApi
+# java bean
+  *create  package name ex<model/bean/dto>
+  * within the package create class ex<Student/user/client>
+  * create variable ex <private int id;/private String fName;>
+  * create constructor
+  * create getter and setter
+
+# Controller
+  * create java class/controller ex<StudentController.java/TestController.java>
+  * use @RestController annotation
+  * create method correspond to desire task ex<GET/POST/PUT/DELETE>
+  * use annotation ex <@GETMAPPING/@POSTMAPPING/@DELETEMAPPING>
+  * return a class/object
---
+++ src/main/java/net/javagauids/springboot/bean/Student.java
@@ -0,0 +1,55 @@
+package net.javagauids.springboot.bean;
+
+public class Student {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private String message;
+
+  //create constructor
+  public Student(int id, String firstName, String lastName, String message) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.message = message;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  
+  //create getter and setters
+  
+
+  
+  
+}
