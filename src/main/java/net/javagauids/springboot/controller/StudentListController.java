e50d960662624d468f3ac24cc2f1139b89487afe
john russelle <kiddogreed@gmail.com>
2022-11-05 23:54:33 +0800

    test posting /create

--- src/main/java/net/javagauids/springboot/controller/StudentListController.java
+++ src/main/java/net/javagauids/springboot/controller/StudentListController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,13 @@ public class StudentListController {
   }
 
 
+  @PostMapping("students/create")
+  public  Student createStudent(@RequestBody Student student){
+    System.out.println(student.getId());
+    System.out.println(student.getFirstName());
+    System.out.println(student.getLastName());
+    System.out.println(student.getMessage());
+    return student;
+  }
   
 }
